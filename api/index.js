f4d5f85dfd5651ffd8b8a2621cd341c908319a8e
twogog <you@example.com>
2024-01-04 20:26:09 +0600

    feat: bot for tests

---
+++ api/index.js
@@ -0,0 +1,23 @@
+const { Telegraf } = require("telegraf");
+const { message } = require("telegraf/filters");
+const bot = new Telegraf(process.env.TOKEN);
+
+bot.start((ctx) =>
+  ctx.reply("Могу рассказать Вам о погоде и актуальном курсе валют на сегодня")
+);
+
+bot.command("currency", async (ctx) => {
+  await getCurrency(ctx);
+});
+
+bot.on(message("text"), async (ctx) => {
+  // console.log(ctx);
+  ctx.reply(
+    'Чтобы узнать погоду, напишите: "Погода город". Если в ответе, указанная страна не совпадает с Вашей, введите название города на английском'
+  );
+});
+
+bot.launch();
+module.exports = async (_, response) => {
+  response.send("OK");
+};
