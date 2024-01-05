ab873cec7cf90f7a45133045f731f3947c55f1da
twogog <you@example.com>
2024-01-06 00:13:44 +0600

    feat: whisper

--- api/index.js
+++ api/index.js
@@ -1,20 +1,52 @@
 const { Telegraf } = require("telegraf");
 const { message } = require("telegraf/filters");
-const bot = new Telegraf(process.env.TOKEN);
+const axios = require("axios");
+const OpenAI = require("openai");
+const fs = require("fs");
+const crypto = require("crypto");
 
-bot.start((ctx) =>
-  ctx.reply("Могу рассказать Вам о погоде и актуальном курсе валют на сегодня")
-);
-
-bot.command("currency", async (ctx) => {
-  await getCurrency(ctx);
+const openai = new OpenAI({
+  apiKey: process.env.AI,
 });
 
+async function main(voice) {
+  const fileName = crypto.randomUUID();
+  const filePath = `/tmp/${fileName}.ogg`;
+  fs.writeFileSync(filePath, voice);
+  const transcription = await openai.audio.transcriptions.create({
+    file: fs.createReadStream(filePath),
+    model: "whisper-1",
+    response_format: "text",
+  });
+  return transcription;
+}
+
+const bot = new Telegraf(process.env.TOKEN);
+
+bot.start((ctx) => ctx.reply("Я создан для обработки аудио сообщений"));
+
 bot.on(message("text"), async (ctx) => {
-  // console.log(ctx);
-  ctx.reply(
-    'Чтобы узнать погоду, напишите: "Погода город". Если в ответе, указанная страна не совпадает с Вашей, введите название города на английском'
-  );
+  ctx.reply("Скопируйте в чат аудио, чтобы получить его в текстовом формате");
+});
+
+bot.on(message("voice"), async (ctx) => {
+  await ctx.telegram
+    .getFileLink(ctx.message.voice.file_id)
+    .then(async (url) => {
+      await axios
+        .get(url, { responseType: "arraybuffer" })
+        .then(async (voice) => {
+          ctx.reply(await main(voice.data));
+        });
+    });
+});
+
+bot.on(message("audio"), async (ctx) => {
+  ctx.telegram.getFileLink(ctx.message.audio.file_id).then((url) => {
+    axios.get(url, { responseType: "arraybuffer" }).then((voice) => {
+      return voice;
+    });
+  });
 });
 
 // bot.launch();
